5b2bcf839590b86338c757a77ec4f1aeab067eaf
klesti@gmail.com <klesti@gmail.com@9804f713-bac4-5a2a-bdb3-9703908ec98e>
2011-10-20 15:24:33 +0000

    Causation scenario:
    1. Define the "network projection"
    2. Generate initial goal and required means

--- trunk/EffectuationCausation/src/EffectuationCausation/CausationBuilder.java
+++ trunk/EffectuationCausation/src/EffectuationCausation/CausationBuilder.java
@@ -4,18 +4,42 @@
 package EffectuationCausation;
 
 import repast.simphony.context.Context;
+import repast.simphony.context.space.graph.NetworkBuilder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.graph.DirectedJungNetwork;
 
 /**
  * @author klesti
  *
  */
-public class CausationBuilder implements ContextBuilder<Object> {
+public class CausationBuilder implements ContextBuilder<Object>  {
 
 	@Override
 	public Context build(Context<Object> context) {
-		// TODO Auto-generated method stub
-		return null;
+		context.setId("causation");
+		
+		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>("entrepreneurial network",
+				context, true);
+		DirectedJungNetwork<Object> network = (DirectedJungNetwork<Object>) netBuilder.buildNetwork();
+		
+		//Add the causator entrepreneur and it's initial goal
+		Causator causator = new Causator(network);
+		Goal initialGoal = new Goal(true);
+		
+		causator.goal = initialGoal;
+		network.addEdge(causator, initialGoal);		
+		
+		context.add(causator);
+		context.add(initialGoal);
+		
+		//Add the required means to the network
+		
+		for (Means m: initialGoal.getRequiredMeans()) {
+			context.add(m);
+			network.addEdge(initialGoal, m);
+		}
+	
+		return context;
 	}
-
+	
 }
