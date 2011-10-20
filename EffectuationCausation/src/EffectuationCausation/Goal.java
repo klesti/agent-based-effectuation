ab0ad066deb07c718e861ec9e2d3972bd00f59d4
klesti@gmail.com <klesti@gmail.com@9804f713-bac4-5a2a-bdb3-9703908ec98e>
2011-10-20 15:24:33 +0000

    Causation scenario:
    1. Define the "network projection"
    2. Generate initial goal and required means

--- EffectuationCausation/src/EffectuationCausation/Goal.java
+++ EffectuationCausation/src/EffectuationCausation/Goal.java
@@ -2,15 +2,58 @@ package EffectuationCausation;
 
 import java.util.List;
 
+import repast.simphony.random.RandomHelper;
+
 public class Goal {
 	private List<Means> requiredMeans;
+	private int[] productVector;
+	
+	public Goal(boolean generateRequiredMeans) {
+		if (generateRequiredMeans) {
+			//TODO: Generate required means
+		}
+	}
+	
+	public Goal() {
+		this(false);
+	}
 
+	public void generateRequiredMeans() {
+		int possibleMeans = RandomHelper.nextIntFromTo(2, 10);		
+		for (int i = 0; i < possibleMeans; i++) {
+			Means m = new Means("Means" + String.valueOf(i));
+			requiredMeans.add(m);
+		}
+	}
+	
+	
+
+	/**
+	 * @return the requiredMeans
+	 */
 	public List<Means> getRequiredMeans() {
 		return requiredMeans;
 	}
 
+	/**
+	 * @param requiredMeans the requiredMeans to set
+	 */
 	public void setRequiredMeans(List<Means> requiredMeans) {
 		this.requiredMeans = requiredMeans;
+	}
+
+	/**
+	 * @return the productVector
+	 */
+	public int[] getProductVector() {
+		return productVector;
+	}
+
+	/**
+	 * @param productVector the productVector to set
+	 */
+	public void setProductVector(int[] productVector) {
+		this.productVector = productVector;
 	}
 
 }
--- EffectuationCausation/src/EffectuationCausation/Means.java
+++ EffectuationCausation/src/EffectuationCausation/Means.java
@@ -1,5 +1,25 @@
 package EffectuationCausation;
 
 public class Means {
+	
+	protected String label;
+	
+	public Means(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
 
 }
