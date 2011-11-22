9ceaef0cdb88c9dfcc3a982deb25f80532308e32
klesti@gmail.com <klesti@gmail.com@9804f713-bac4-5a2a-bdb3-9703908ec98e>
2011-11-22 17:19:59 +0000

    First steps towards the effectuation scenario implementation

--- EffectuationCausation/src/EffectuationCausation/EffectuationBuilder.java
+++ EffectuationCausation/src/EffectuationCausation/EffectuationBuilder.java
@@ -4,18 +4,51 @@
 package EffectuationCausation;
 
 import repast.simphony.context.Context;
+import repast.simphony.context.DefaultContext;
+import repast.simphony.context.space.graph.NetworkBuilder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.graph.Network;
 
 /**
  * @author klesti
  *
  */
-public class EffectuationBuilder implements ContextBuilder<Object> {
+public class EffectuationBuilder extends DefaultContext<Object> implements ContextBuilder<Object> {
 
+	//Parameters, later will be prodived by the user interface
+	public static final int vectorSpaceSize = 10;
+	public static final int numberOfCustomers = 100;	
+	public static final int maxInitialGoals = 5;
+	public static Network<Object> network;
+	
 	@Override
 	public Context build(Context<Object> context) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		context.setId("effectuation");
+		
+		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>("effectuation network",
+				context, true);
+		
+		network  = netBuilder.buildNetwork();	
+		
+		//Add the effectuator entrepreneur and it's initial goal(s)
+		Effectuator effectuator = new Effectuator(network, "Effectuator");
+		context.add(effectuator);
+		
+		int totalInitialGoals = RandomHelper.nextIntFromTo(1, maxInitialGoals);
+		
+		for (int i = 0; i < totalInitialGoals; i++) {
+			Goal g = new Goal(vectorSpaceSize);
+			context.add(g);
+			network.addEdge(effectuator, g);
+		}
+		
+		//Network generation
+		
+		
+
+		return context;
 	}
 
 }
