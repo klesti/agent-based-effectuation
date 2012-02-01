78190e02b3a3cee0010e6493d984c9e871152ee8
klesti@gmail.com <klesti@gmail.com@9804f713-bac4-5a2a-bdb3-9703908ec98e>
2012-02-01 18:17:31 +0000

    Refactored the BarabasiAlbertNetworkGenerator (extracted a subclass)

--- EffectuationCausation/src/EffectuationCausation/BarabasiAlbertNetworkGenerator.java
+++ EffectuationCausation/src/EffectuationCausation/BarabasiAlbertNetworkGenerator.java
@@ -13,22 +13,10 @@ import repast.simphony.space.graph.Network;
  * @author klesti
  *
  */
-public class BarabasiAlbertNetworkGenerator implements NetworkGenerator<Object> {
+public class BarabasiAlbertNetworkGenerator extends EntrepreneurialNetworkGenerator implements NetworkGenerator<Object> {
 	
-	private int edgesPerStep;
-	private int totalCustomers;
-	private int totalEntrepreneuers;
-	private int totalInvestors;
-	private Context<Object> context;
-	private Network<Object> network;
-
-	public BarabasiAlbertNetworkGenerator(Context<Object> context) {		
-		this.context = context;
-		
-		edgesPerStep = 0;
-		totalCustomers = 0;
-		totalEntrepreneuers = 0;
-		totalInvestors = 0;
+	public BarabasiAlbertNetworkGenerator(Context<Object> context) {
+		super(context);
 	}
 
 	/* (non-Javadoc)
@@ -65,6 +53,7 @@ public class BarabasiAlbertNetworkGenerator implements NetworkGenerator<Object> 
 		return network;
 	}
 	
+	
 	/**
 	 * Preferential attachment
 	 * @param n Node to be attached
@@ -93,137 +82,4 @@ public class BarabasiAlbertNetworkGenerator implements NetworkGenerator<Object> 
 			}
 		}
 	}
-	
-	
-	/**
-	 * Initializes the network
-	 * @param p initial wiring probability
-	 */
-	private void initializeNetwork(double p) {
-		if (totalCustomers == 0 && totalEntrepreneuers == 0 && totalInvestors == 0) {
-			initializeParametersRandomly();
-		}
-		
-		context.add(new Customer(context, network, "Customer" + String.valueOf(RandomHelper.nextInt())));
-		totalCustomers--;
-		context.add(new Customer(context, network, "Customer" + String.valueOf(RandomHelper.nextInt())));
-		totalCustomers--;
-
-		Entrepreneur e1 = new Entrepreneur(context, network, "Entrepreneur" + String.valueOf(RandomHelper.nextInt()));
-		e1.generateGoal();
-		totalEntrepreneuers--;
-		
-		Entrepreneur e2 = new Entrepreneur(context, network, "Entrepreneur" + String.valueOf(RandomHelper.nextInt()));
-		e2.generateGoal();
-		context.add(e2);		
-		totalEntrepreneuers--;
-		
-		context.add(new Investor(context, network, "Investor" + String.valueOf(RandomHelper.nextInt())));
-		totalInvestors--;
-		context.add(new Investor(context, network, "Investor" + String.valueOf(RandomHelper.nextInt())));
-		totalInvestors--;
-		
-		//Initial wiring using a random network
-		for (Object i: network.getNodes()) {
-			for (Object j: network.getNodes()) {
-				double random = RandomHelper.nextDoubleFromTo(0, 1);
-				if (i.getClass().isAssignableFrom(Agent.class) &&
-						j.getClass().isAssignableFrom(Agent.class) &&
-						random >= p) {
-					network.addEdge(i, j);					
-				}
-			}
-		}
-	}
-	
-	public void initializeParametersRandomly() {
-		edgesPerStep = RandomHelper.nextIntFromTo(2, 5);
-		totalCustomers = RandomHelper.nextIntFromTo(5, 50);
-		totalEntrepreneuers = RandomHelper.nextIntFromTo(2, 10);
-		totalInvestors = RandomHelper.nextIntFromTo(2, 5);
-	}
-	
-	/**
-	 * @return the edgesPerStep
-	 */
-	public int getEdgesPerStep() {
-		return edgesPerStep;
-	}
-
-	/**
-	 * @param edgesPerStep the edgesPerStep to set
-	 */
-	public void seEdgesPerStep(int edgesPerStep) {
-		this.edgesPerStep = edgesPerStep;
-	}
-
-	/**
-	 * @return the totalCustomers
-	 */
-	public int getTotalCustomers() {
-		return totalCustomers;
-	}
-
-	/**
-	 * @param totalCustomers the totalCustomers to set
-	 */
-	public void setTotalCustomers(int totalCustomers) {
-		this.totalCustomers = totalCustomers;
-	}
-
-	/**
-	 * @return the totalEntrepreneuers
-	 */
-	public int getTotalEntrepreneuers() {
-		return totalEntrepreneuers;
-	}
-
-	/**
-	 * @param totalEntrepreneuers the totalEntrepreneuers to set
-	 */
-	public void setTotalEntrepreneuers(int totalEntrepreneuers) {
-		this.totalEntrepreneuers = totalEntrepreneuers;
-	}
-
-	/**
-	 * @return the totalInvestors
-	 */
-	public int getTotalInvestors() {
-		return totalInvestors;
-	}
-
-	/**
-	 * @param totalInvestors the totalInvestors to set
-	 */
-	public void setTotalInvestors(int totalInvestors) {
-		this.totalInvestors = totalInvestors;
-	}
-
-	/**
-	 * @return the context
-	 */
-	public Context<Object> getContext() {
-		return context;
-	}
-
-	/**
-	 * @param context the context to set
-	 */
-	public void setContext(Context<Object> context) {
-		this.context = context;
-	}
-
-	/**
-	 * @return the network
-	 */
-	public Network<Object> getNetwork() {
-		return network;
-	}
-
-	/**
-	 * @param network the network to set
-	 */
-	public void setNetwork(Network<Object> network) {
-		this.network = network;
-	}	
 }
---
+++ EffectuationCausation/src/EffectuationCausation/EntrepreneurialNetworkGenerator.java
@@ -0,0 +1,158 @@
+package EffectuationCausation;
+
+import repast.simphony.context.Context;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.graph.Network;
+
+public class EntrepreneurialNetworkGenerator {
+
+	protected int edgesPerStep;
+	protected int totalCustomers;
+	protected int totalEntrepreneuers;
+	protected int totalInvestors;
+	protected Context<Object> context;
+	protected Network<Object> network;
+
+	public EntrepreneurialNetworkGenerator(Context<Object> context) {
+		this.context = context;
+		
+		edgesPerStep = 0;
+		totalCustomers = 0;
+		totalEntrepreneuers = 0;
+		totalInvestors = 0;
+	}
+
+	
+	/**
+	 * Initializes the network
+	 * @param p initial wiring probability
+	 */
+	protected void initializeNetwork(double p) {
+		if (totalCustomers == 0 && totalEntrepreneuers == 0 && totalInvestors == 0) {
+			initializeParametersRandomly();
+		}
+		
+		context.add(new Customer(context, network, "Customer" + String.valueOf(RandomHelper.nextInt())));
+		totalCustomers--;
+		context.add(new Customer(context, network, "Customer" + String.valueOf(RandomHelper.nextInt())));
+		totalCustomers--;
+	
+		Entrepreneur e1 = new Entrepreneur(context, network, "Entrepreneur" + String.valueOf(RandomHelper.nextInt()));
+		e1.generateGoal();
+		totalEntrepreneuers--;
+		
+		Entrepreneur e2 = new Entrepreneur(context, network, "Entrepreneur" + String.valueOf(RandomHelper.nextInt()));
+		e2.generateGoal();
+		context.add(e2);		
+		totalEntrepreneuers--;
+		
+		context.add(new Investor(context, network, "Investor" + String.valueOf(RandomHelper.nextInt())));
+		totalInvestors--;
+		context.add(new Investor(context, network, "Investor" + String.valueOf(RandomHelper.nextInt())));
+		totalInvestors--;
+		
+		//Initial wiring using a random network
+		for (Object i: network.getNodes()) {
+			for (Object j: network.getNodes()) {
+				double random = RandomHelper.nextDoubleFromTo(0, 1);
+				if (i.getClass().isAssignableFrom(Agent.class) &&
+						j.getClass().isAssignableFrom(Agent.class) &&
+						random >= p) {
+					network.addEdge(i, j);					
+				}
+			}
+		}
+	}
+	
+	public void initializeParametersRandomly() {
+		edgesPerStep = RandomHelper.nextIntFromTo(2, 5);
+		totalCustomers = RandomHelper.nextIntFromTo(5, 50);
+		totalEntrepreneuers = RandomHelper.nextIntFromTo(2, 10);
+		totalInvestors = RandomHelper.nextIntFromTo(2, 5);
+	}	
+
+	/**
+	 * @return the edgesPerStep
+	 */
+	public int getEdgesPerStep() {
+		return edgesPerStep;
+	}
+
+	/**
+	 * @param edgesPerStep the edgesPerStep to set
+	 */
+	public void seEdgesPerStep(int edgesPerStep) {
+		this.edgesPerStep = edgesPerStep;
+	}
+
+	/**
+	 * @return the totalCustomers
+	 */
+	public int getTotalCustomers() {
+		return totalCustomers;
+	}
+
+	/**
+	 * @param totalCustomers the totalCustomers to set
+	 */
+	public void setTotalCustomers(int totalCustomers) {
+		this.totalCustomers = totalCustomers;
+	}
+
+	/**
+	 * @return the totalEntrepreneuers
+	 */
+	public int getTotalEntrepreneuers() {
+		return totalEntrepreneuers;
+	}
+
+	/**
+	 * @param totalEntrepreneuers the totalEntrepreneuers to set
+	 */
+	public void setTotalEntrepreneuers(int totalEntrepreneuers) {
+		this.totalEntrepreneuers = totalEntrepreneuers;
+	}
+
+	/**
+	 * @return the totalInvestors
+	 */
+	public int getTotalInvestors() {
+		return totalInvestors;
+	}
+
+	/**
+	 * @param totalInvestors the totalInvestors to set
+	 */
+	public void setTotalInvestors(int totalInvestors) {
+		this.totalInvestors = totalInvestors;
+	}
+
+	/**
+	 * @return the context
+	 */
+	public Context<Object> getContext() {
+		return context;
+	}
+
+	/**
+	 * @param context the context to set
+	 */
+	public void setContext(Context<Object> context) {
+		this.context = context;
+	}
+
+	/**
+	 * @return the network
+	 */
+	public Network<Object> getNetwork() {
+		return network;
+	}
+
+	/**
+	 * @param network the network to set
+	 */
+	public void setNetwork(Network<Object> network) {
+		this.network = network;
+	}
+
+}
